ce029e80744031a9d4e1d33cd79928f2f25c96df
Daniel Sarmiento <danielsarmientoenterprise@gmail.com>
2024-01-04 10:49:34 -0500

    change endpoint

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -4,45 +4,60 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NgZone } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
 
-
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent {
-  public products: any = []
+  public products: any = [];
   public product: any;
 
   public nameProduct: any;
-  constructor(public httpClient: HttpClient, private route: ActivatedRoute, private router: Router, private ngZone: NgZone, private cdRef: ChangeDetectorRef) { }
+  constructor(
+    public httpClient: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router,
+    private ngZone: NgZone,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     console.log();
-    this.getData()
+    this.getData();
   }
-  getData(name = "") {
+  getData(name = '') {
     this.product = {};
     if (name) {
-      this.nameProduct = name
+      this.nameProduct = name;
     } else {
-      this.nameProduct = this.route.snapshot.params['name'].replace(' ', '%20')
+      this.nameProduct = this.route.snapshot.params['name'].replace(' ', '%20');
     }
-    this.httpClient.get('http://localhost:5000/product/' + this.nameProduct).subscribe((res: any) => {
-      this.ngZone.run(() => {
-        this.product = res;
-        console.log(this.product)
-      });
-    });
-    this.httpClient.get('http://localhost:5000/recomendar/' + this.nameProduct).subscribe((res: any) => {
-      this.ngZone.run(() => {
-        this.products = res.sort(function (a: any, b: any) {
-          return parseFloat(b.similarity) - parseFloat(a.similarity);
+    this.httpClient
+      .get(
+        'https://backend-suggest-jmn5bhj2y-backsoul.vercel.app/product/' +
+          this.nameProduct
+      )
+      .subscribe((res: any) => {
+        this.ngZone.run(() => {
+          this.product = res;
+          console.log(this.product);
         });
-
-        this.products = this.products.filter((a: any) => a.similarity != 1);
       });
-    });
+    this.httpClient
+      .get(
+        'https://backend-suggest-jmn5bhj2y-backsoul.vercel.app/recomendar/' +
+          this.nameProduct
+      )
+      .subscribe((res: any) => {
+        this.ngZone.run(() => {
+          this.products = res.sort(function (a: any, b: any) {
+            return parseFloat(b.similarity) - parseFloat(a.similarity);
+          });
+
+          this.products = this.products.filter((a: any) => a.similarity != 1);
+        });
+      });
   }
   get getProduct() {
     return this.product;
@@ -53,20 +68,20 @@ export class DetailComponent {
   }
 
   goToHome() {
-    this.router.navigate([''])
+    this.router.navigate(['']);
   }
   changeBackground(): any {
     if (this.product) {
-      return { 'background': "url(" + this.product.image + ")" };
+      return { background: 'url(' + this.product.image + ')' };
     } else {
       return;
     }
   }
   loading(): any {
     if (this.product != null) {
-      return { 'filter': "brightness(1)" };
+      return { filter: 'brightness(1)' };
     } else {
-      return { 'filter': "brightness(0.7)" };
+      return { filter: 'brightness(0.7)' };
     }
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,20 +5,22 @@ import { Route, Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  public products:any = []
+  public products: any = [];
 
-  constructor(public httpClient: HttpClient, private route:Router){}
+  constructor(public httpClient: HttpClient, private route: Router) {}
 
-  ngOnInit(){
-    this.httpClient.get('http://localhost:5000/products').subscribe((res: any)=>{
-            this.products = res;
-        });
+  ngOnInit() {
+    this.httpClient
+      .get('https://backend-suggest-jmn5bhj2y-backsoul.vercel.app/products')
+      .subscribe((res: any) => {
+        this.products = res;
+      });
   }
 
-  goToDetail(name: string){
-    this.route.navigate([name])
+  goToDetail(name: string) {
+    this.route.navigate([name]);
   }
 }
